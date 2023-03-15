3dd178d3515e62a458318dedaecefb3c88882edb
Gjade912 <gustaf@jaderb.se>
2023-03-15 22:07:20 +0100

    start routes

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use("/products", require("./routes/productsRoute"));
 
 
 
